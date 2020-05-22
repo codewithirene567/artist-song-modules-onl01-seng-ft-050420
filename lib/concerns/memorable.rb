ad55b1d23572ed28fe8f0541313cd7bdc9547c40
Irene Scott <irenejinscott@gmail.com>
2020-05-22 20:41:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
 module ClassMethods
   def reset_all
-  self.all.clear
+  all.clear
   end
 
   def count
-  self.all.count
+  all.count
   end
 end
   #self keyword is omitted when defining class methods inside modules
