192bcbd29565dec6497297324a705bef42e509b0
Irene Scott <irenejinscott@gmail.com>
2020-05-22 20:23:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,10 @@ class Song
 extend Memorable::ClassMethods
 include Paramable
 
+extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
 #extend Memorable::ClassMethods
   #extend Findable::ClassMethods
   #include Memorable::InstanceMethods
