af94449edb1f7ba82b805d93c8c8c663b2b19ed8
Irene Scott <irenejinscott@gmail.com>
2020-05-22 21:13:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,14 +8,14 @@ include Paramable::InstanceMethods
 
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  
+
     attr_accessor :name
     attr_reader :artist
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  #def initialize
+  #  @@songs << self
+#  end
 
 
 
