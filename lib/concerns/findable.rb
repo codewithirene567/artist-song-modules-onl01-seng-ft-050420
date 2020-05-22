d740040d4e9ada2e99e129e764fe00449e014e98
Irene Scott <irenejinscott@gmail.com>
2020-05-22 19:48:37 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,7 @@
 module Findable
   module InstanceMethods
   def find_by_name
-    def find_by_name(name)
-      @@artists.detect{|a| a.name == name}
-    end
+    
 
     def find_by_name(name)
       @@songs.detect{|a| a.name == name}
