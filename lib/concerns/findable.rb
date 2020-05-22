16ca7a6032da99bb4ec490efcd4b37d8b80393ed
Irene Scott <irenejinscott@gmail.com>
2020-05-22 20:59:02 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,12 +1,9 @@
 require "pry"
 module Findable
-  def find_by_name(name)
-    binding.pry
-     all.detect{|a| a.name == name}
-   end
 
   module ClassMethods
     def find_by_name(name)
+      binding.pry
       all.detect{|a| a.name == name}
     end
 
