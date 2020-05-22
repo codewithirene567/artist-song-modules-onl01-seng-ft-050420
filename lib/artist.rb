298651a674742f42772d5cdd62ba6be93f7d8d05
Irene Scott <irenejinscott@gmail.com>
2020-05-22 19:41:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 class Artist
-
-  
-extend Memorable::ClassMethods
-  include Paramable::InstanceMethods
+  extend Memorable
+  include Paramable
 
   attr_accessor :name
   attr_reader :songs
@@ -16,13 +14,21 @@ extend Memorable::ClassMethods
     @songs = []
   end
 
-
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
   end
 
+  def self.reset_all
+    self.all.clear
+  end
 
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
@@ -33,9 +39,7 @@ extend Memorable::ClassMethods
     songs.each { |song| add_song(song) }
   end
 
-
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
-
-#string = "We have many dogs"
-#string.gsub("dogs", "cats")
-# "We have many cats"
